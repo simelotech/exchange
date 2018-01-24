d18ef94cdd075ba77b6439a9c86353b4041b3c72
alek6 <dev0023@simelo.tech>
2018-01-24 01:34:12 -0800

    added blockchain api calls to create wallet and return its address (with mock response)

--- exchange_app/api_1_0/wallets.py
+++ exchange_app/api_1_0/wallets.py
@@ -1,14 +1,28 @@
 from flask import request, jsonify, abort
 from . import api
-
+from .blockchain import get_url, post_url
+import json
 
 @api.route('/wallets', methods=['POST'])
 def wallets():
     """
     """
-    # TODO: Create wallet using blockchain api and return its address
-    address = "new_wallet_address"
-    return jsonify({"address": address})
+    # generate new seed first
+	new_seed = get_url("/wallet/newSeed") 
+	new_seed = {"seed": "helmet van actor peanut"} #TODO: revove this mock response
+	
+	# create the wallet from seed
+	values = {"seed": new_seed["seed"], "label": "wallet123", "scan": "5"} #TODO: Where to get labels? How about scan?
+	new_wallet = post_url("/wallet/create", values)
+	new_wallet = {"meta":{"coin": "sky", "filename": "2018-01-24-d554.wlt"}, #TODO: should we store filenames?
+				  "entries":[{
+					"address": "addressjdjebcjdhbjehc", 
+					"public_key": "publicdwewewvefvfv",
+					"secret_key": "privatewthbregvefvwef"}
+					]} #TODO: remove this mock response.
+	
+	
+    return jsonify({"address": new_wallet["entries"][0]["address"]})
 
 
 @api.route('/wallets/<string:address>/cashout', methods=['POST'])
