32b823128515255972f1725292f66ba2b0ce9d95
alek6 <dev0023@simelo.tech>
2018-01-26 02:24:45 -0800

    create transfer fund function. Make specific function for blockchain api calls. Endpoints should use specific functions instead of generic get/post when possible

--- exchange_app/api_1_0/blockchain.py
+++ exchange_app/api_1_0/blockchain.py
@@ -43,3 +43,38 @@ def post_url(path, values = ""):
 	response_data = {"Called": "post_url()", "url": url, "values:": values}
 
 	return response_data
+	
+		
+def create_wallet():
+	"""
+	"""
+	
+	# generate new seed first
+	#new_seed = requests.get(form_url(base_url, "/wallet/newSeed")).json()
+	new_seed = {"seed": "helmet van actor peanut"} #TODO: revove this mock response
+	
+	# create the wallet from seed
+	values = {"seed": new_seed["seed"], "label": "wallet123", "scan": "5"} #TODO: Where to get labels? How about scan?
+	#new_wallet = requests.post(form_url(base_url, "/wallet/newSeed"), values).json()
+	new_wallet = {"meta":{"coin": "sky", "filename": "2018-01-24-d554.wlt"}, #TODO: should we store filenames?
+				  "entries":[{
+					"address": "addressjdjebcjdhbjehc", 
+					"public_key": "publicdwewewvefvfv",
+					"secret_key": "privatewthbregvefvwef"}
+					]} #TODO: remove this mock response.
+	
+	result = {"address": new_wallet["entries"][0]["address"]}
+	
+	return result #TODO: Error handling
+	
+	
+def spend(values):
+	"""
+	"""
+	#resp = requests.post(form_url(base_url, "/wallets/cashout"), data = values)
+	result = resp.status_code
+	result = 200
+	
+	return result #TODO: Error handling. Sometimes there is 200 response but there is error code in json
+
+	
--- exchange_app/api_1_0/wallets.py
+++ exchange_app/api_1_0/wallets.py
@@ -1,45 +1,39 @@
 from flask import request, jsonify, abort
 from . import api
-from .blockchain import get_url, post_url
+from .blockchain import spend, create_wallet
 import json
 
 @api.route('/wallets', methods=['POST'])
 def wallets():
 	"""
 	"""
-	# generate new seed first
-	new_seed = get_url("/wallet/newSeed") 
-	new_seed = {"seed": "helmet van actor peanut"} #TODO: revove this mock response
 	
-	# create the wallet from seed
-	values = {"seed": new_seed["seed"], "label": "wallet123", "scan": "5"} #TODO: Where to get labels? How about scan?
-	new_wallet = post_url("/wallet/create", values)
-	new_wallet = {"meta":{"coin": "sky", "filename": "2018-01-24-d554.wlt"}, #TODO: should we store filenames?
-				  "entries":[{
-					"address": "addressjdjebcjdhbjehc", 
-					"public_key": "publicdwewewvefvfv",
-					"secret_key": "privatewthbregvefvwef"}
-					]} #TODO: remove this mock response.
+	result = create_wallet()
 	
-	
-	return jsonify({"address": new_wallet["entries"][0]["address"]})
+	return jsonify(result)
 
 
 @api.route('/wallets/<string:address>/cashout', methods=['POST'])
 def wallets_cashout(address):
-    """
-    Record transaction in blockchain from wallet address to destination address in json request
-    Validate request json
-    """
-    if not request.json \
-            or "operationId" not in request.json \
-            or "to" not in request.json \
-            or "assetId" not in request.json \
-            or "amount" not in request.json:
-        abort(400)  # Bad request
-    # TODO: send transfer to blockchain and get result.
-    result = True
+	"""
+	Record transaction in blockchain from wallet address to destination address in json request
+	Validate request json
+	"""
+	if not request.json \
+			or "operationId" not in request.json \
+			or "to" not in request.json \
+			or "assetId" not in request.json \
+			or "amount" not in request.json:
+		abort(400)  # Bad request
+	
+	values = {}
+	values["id"] = address
+	values["dst"] = request.json["to"]
+	values["coins"] = request.json["amount"]
+	
+	result = spend(values)
 
-    if not result:
-        abort(500)  # internal server error
-    return ""
+	if result != 200:
+		abort(result)  # internal server error
+	
+	return ""
