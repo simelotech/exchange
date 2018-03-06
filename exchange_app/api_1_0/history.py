a5a72473227731dfb36d8156eed45d9fa8a46be9
alek6 <dev0023@simelo.tech>
2018-03-06 00:52:47 -0500

    added history.py with initial code for #40, #41, #42, #43, #44, #49

---
+++ exchange_app/api_1_0/history.py
@@ -0,0 +1,61 @@
+from flask import request, jsonify, make_response
+from . import api
+from .common import build_error, generate_hash_key
+import logging
+from .. import app
+
+
+@api.route('/transactions/history/from/<string:address>', methods=['GET'])
+def get_history_from_address(address):
+    """
+    Returns completed transactions that transfer fund from the address 
+    """
+    
+    return make_response(jsonify(build_error("Not implemented get_history_from_address")), 200)
+    
+    
+@api.route('/transactions/history/to/<string:address>', methods=['GET'])
+def get_history_to_address(address):
+    """
+    Returns completed transactions that transfer fund to the address 
+    """
+    
+    return make_response(jsonify(build_error("Not implemented get_history_to_address")), 200)
+    
+    
+@api.route('/transactions/history/from/<string:address>/observation', methods=['DELETE'])
+def del_history_from_address(address):
+    """
+    Stops observation of the transactions that transfer fund from the address
+    """
+    
+    return make_response(jsonify(build_error("Not implemented del_history_from_address")), 200)
+    
+    
+@api.route('/transactions/history/to/<string:address>/observation', methods=['DELETE'])
+def del_history_to_address(address):
+    """
+    Stops observation of the transactions that transfer fund to the address 
+    """
+    
+    return make_response(jsonify(build_error("Not implemented del_history_to_address")), 200)
+    
+
+@api.route('/transactions/history/from/<string:address>/observation', methods=['POST'])
+def add_history_from_address(address):
+    """
+    Starts observation of the transactions that transfer fund from the address 
+    """
+    
+    return make_response(jsonify(build_error("Not implemented add_history_from_address")), 200)
+    
+    
+@api.route('/transactions/history/to/<string:address>/observation', methods=['POST'])
+def add_history_to_address(address):
+    """
+    Starts observation of the transactions that transfer fund from the address
+    """
+    
+    return make_response(jsonify(build_error("Not implemented add_history_to_address")), 200)
+    
+
