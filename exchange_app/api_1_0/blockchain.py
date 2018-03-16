1b5e0afcea7f86ee4e3385f104a19bc30ee09e5d
alek6 <dev0023@simelo.tech>
2018-03-16 01:24:57 -0400

    Create get transactions from/to address in blockchain interface (returns mock data for now). Created get transfers from/to in history endpoint file

--- exchange_app/api_1_0/blockchain.py
+++ exchange_app/api_1_0/blockchain.py
@@ -132,3 +132,60 @@ def get_balance(address):
         logging.debug(balances.json())
 
     return balances.json()['confirmed']['coins']
+    
+    
+def get_transactions_from(address, afterhash):
+    """
+    return all transactions from address after the one specified by afterhash
+    """
+    
+    #TODO: Read this from blockchain
+    
+    transfers = [
+        {"operationId": "guid", #TODO: Where to get this. If is only valid for this app's transactions, when do we generate/store it? Can blockchain provide it?
+         "timestamp": "20071103T161805Z", #TODO: confirm if should use ISO-8601 basic or extended timestamp representation
+         "fromAddress": address,
+         "toAddress": "abcd1234",
+         "assetId": "skycoin"
+         "amount": "1000000",
+         "hash": "qwertyasdfg"
+        }, 
+        {"operationId": "guid", 
+         "timestamp": "20180215T231403Z", 
+         "fromAddress": address,
+         "toAddress": "efgh5678",
+         "assetId": "skycoin"
+         "amount": "2000000",
+         "hash": "asdfgzxcvb"
+        }
+    ]
+    
+    return transfers
+    
+def get_transactions_to(address, afterhash):
+    """
+    return all transactions to address after the one specified by afterhash
+    """
+    
+    #TODO: Read this from blockchain
+    
+    transfers = [
+        {"operationId": "guid", #TODO: Where to get this. If is only valid for this app's transactions, when do we generate/store it? Can blockchain provide it?
+         "timestamp": "20071103T161805Z", #TODO: confirm if should use ISO-8601 basic or extended timestamp representation
+         "fromAddress": "abcd1234",
+         "toAddress": address,
+         "assetId": "skycoin"
+         "amount": "1000000",
+         "hash": "qwertyasdfg"
+        }, 
+        {"operationId": "guid", 
+         "timestamp": "20180215T231403Z", 
+         "fromAddress": "efgh5678",
+         "toAddress": address,
+         "assetId": "skycoin"
+         "amount": "2000000",
+         "hash": "asdfgzxcvb"
+        }
+    ]
+    
+    return transfers
--- exchange_app/api_1_0/history.py
+++ exchange_app/api_1_0/history.py
@@ -1,6 +1,7 @@
 from flask import request, jsonify, make_response
-from . import api
+from . import 
 from .common import build_error, generate_hash_key
+from .blockchain.py import get_transactions_from, get_transactions_to
 from .redis_interface import get_cont_address_transfers_from, set_cont_address_transfers_from, del_cont_address_transfers_from
 from .redis_interface import get_cont_address_transfers_to, set_cont_address_transfers_to, del_cont_address_transfers_to
 import logging
@@ -12,67 +13,20 @@ def get_history_from_address(address):
     """
     Returns completed transactions that transfer fund from the address 
     """
+    
     take = request.args.get('take')
     if take is None:
         take = 0
     else:
         take = int(take)
     
-    continuation = request.args.get('continuation')
-    if continuation is None:
-        continuation = ""
-    
-    #get continuation address if continuation context is set
-    cont_address = ""
-    if continuation != "":
-        cont_address = get_cont_address_transfers_from(continuation) #get the continuation address from redis
-        
-    
-    #Get address list from mongodb
-    addresses = get_addresses_transfers_observation_from()  
+    afterhash = request.args.get('afterHash')
+    if afterhash is None:
+        afterhash = ""  
+       
+    items = get_transactions_to(addr, afterhash)
 
-    if app.config['DEBUG']:
-        logging.debug("addresses")
-        logging.debug(addresses)
-    
-    items = []
-    
-    #define search boundaries
-    start_index = 0 if cont_address == "" or cont_address not in addresses else addresses.index(cont_address)
-    total_items = take if take != 0 else len(addresses)   
-
-    while len(items) < total_items and start_index < len(addresses):
-        item = {}
-        
-        if app.config['DEBUG']:
-            logging.debug("Start Index: %i", start_index)
-            logging.debug("Total Items: %i", total_items)
-            logging.debug("address: %s", addresses[start_index])
-            
-        item['address'] = addresses[start_index]
-        item['assetId'] = 0
-        item['balance'] = 4# get_balance(addr) #TODO: uncomment get balances when finish testing. Otherwise nothing will be returned
-        item['block'] = 0 #TODO: where to get block sequence?
-        if item['balance'] != 0:
-            items.append(item)
-        start_index += 1
-
-    #Save continuation address in Redis
-    if start_index < len(addresses): #Still data to read        
-        #If it is the first call and need continuation create the token
-        if continuation == "" and take != 0 and take < len(addresses):
-            continuation = generate_hash_key()        
-        set_cont_address_transfers_from(continuation, addresses[start_index])
-    else:
-        del_cont_address_transfers_from(continuation)
-        continuation = ""
-
-    response = {"continuation": continuation, "items": items}
-    
-    if app.config['DEBUG']:
-        logging.debug("Got balances from observation list")
-        logging.debug(items)
-        
+    response = items if take == 0 else items[0:take]
 
     return jsonify(response)
     
@@ -89,62 +43,13 @@ def get_history_to_address(address):
     else:
         take = int(take)
     
-    continuation = request.args.get('continuation')
-    if continuation is None:
-        continuation = ""
-    
-    #get continuation address if continuation context is set
-    cont_address = ""
-    if continuation != "":
-        cont_address = get_cont_address_transfers_to(continuation) #get the continuation address from redis
-        
-    
-    #Get address list from mongodb
-    addresses = get_addresses_transfers_observation_from()  
+    afterhash = request.args.get('afterHash')
+    if afterhash is None:
+        afterhash = ""  
+       
+    items = get_transactions_to(addr, afterhash)
 
-    if app.config['DEBUG']:
-        logging.debug("addresses")
-        logging.debug(addresses)
-    
-    items = []
-    
-    #define search boundaries
-    start_index = 0 if cont_address == "" or cont_address not in addresses else addresses.index(cont_address)
-    total_items = take if take != 0 else len(addresses)   
-
-    while len(items) < total_items and start_index < len(addresses):
-        item = {}
-        
-        if app.config['DEBUG']:
-            logging.debug("Start Index: %i", start_index)
-            logging.debug("Total Items: %i", total_items)
-            logging.debug("address: %s", addresses[start_index])
-            
-        item['address'] = addresses[start_index]
-        item['assetId'] = 0
-        item['balance'] = 4# get_balance(addr) #TODO: uncomment get balances when finish testing. Otherwise nothing will be returned.
-        #TODO: Handle case when address is deleted during paging read
-        item['block'] = 0 #TODO: where to get block sequence?
-        if item['balance'] != 0:
-            items.append(item)
-        start_index += 1
-
-    #Save continuation address in Redis
-    if start_index < len(addresses): #Still data to read        
-        #If it is the first call and need continuation create the token
-        if continuation == "" and take != 0 and take < len(addresses):
-            continuation = generate_hash_key()        
-        set_cont_address_transfers_to(continuation, addresses[start_index])
-    else:
-        del_cont_address_transfers_to(continuation)
-        continuation = ""
-
-    response = {"continuation": continuation, "items": items}
-    
-    if app.config['DEBUG']:
-        logging.debug("Got balances from observation list")
-        logging.debug(items)
-        
+    response = items if take == 0 else items[0:take]
 
     return jsonify(response)
     
