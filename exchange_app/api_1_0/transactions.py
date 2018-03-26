2e31d3fc0ff33d7cd392a29cb9a58951bd04d592
Ozkar L. Garcell <dev0028@simelo.tech>
2018-03-26 15:42:24 -0400

    many_inputs_tx api callback

---
+++ exchange_app/api_1_0/transactions.py
@@ -0,0 +1,18 @@
+from flask import request, jsonify, make_response
+
+from . import api
+from .blockchain import transaction_many_inputs
+from .common import build_error
+
+
+@api.route('/api/transactions/many-inputs', methods=['POST'])
+def transactions_many_inputs():
+    if not request.json:
+        return make_response(jsonify(build_error("Input format error")), 400)
+    params = {'operationID', 'inputs', 'toAddress', 'assetID'}
+    if all(x not in params for x in request.json):
+        return make_response(jsonify(build_error("Input data error")), 400)
+    result = transaction_many_inputs(request.json)
+    if "transactionContext" in result:
+        return jsonify(result)
+    return jsonify({"status": 500, "error": "Invalid response"})
