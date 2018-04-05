b7439efd6eda7bcd220c7aa6465787980aa4c4c0
Ozkar L. Garcell <dev0028@simelo.tech>
2018-04-05 15:46:42 -0400

    added persistence to tx with many outputs refs #33

--- exchange_app/api_1_0/transactions.py
+++ exchange_app/api_1_0/transactions.py
@@ -1,8 +1,11 @@
+import logging
 from flask import request, jsonify, make_response
 
 from . import api
 from .blockchain import transaction_many_inputs, transaction_many_outputs
 from .common import build_error
+from ..models import add_many_outputs_tx
+from .. import app
 
 
 @api.route('/api/transactions/many-inputs', methods=['POST'])
@@ -22,10 +25,14 @@ def transactions_many_inputs():
 def transactions_many_outputs():
     if not request.json:
         return make_response(jsonify(build_error("Input format error")), 400)
-    params = {'operationID', 'fromAddress', 'fromAddressContext', 'outputs', 'assetId'}
+    params = {'operationId', 'fromAddress', 'fromAddressContext', 'outputs', 'assetId'}
     if all(x not in params for x in request.json):
         return make_response(jsonify(build_error("Input data error")), 400)
-    result = transaction_many_outputs(request.json)
-    if "transactionContext" in result:
-        return jsonify(result)
+    tx = add_many_outputs_tx(request.json)
+    if tx:
+        result = transaction_many_outputs(request.json)
+        if "transactionContext" in result:
+            return jsonify(result)
+        if app.config['DEBUG']:
+            logging.debug("Transaction: %s", request.args.get('operationId'))
     return jsonify({"status": 500, "error": "Invalid response"})
--- exchange_app/models.py
+++ exchange_app/models.py
@@ -1,5 +1,6 @@
 import logging
 from exchange_app import mongo, app
+from bson.objectid import ObjectId
 
 
 def store_wallet(document):
@@ -68,3 +69,19 @@ def exists_address_observation(address):
         return True
     else:
         return False
+
+
+def add_many_outputs_tx(tx):
+    """
+    Add not signed transaction with many outputs
+    """
+    many_inputs = mongo.db.many_outputs_tx # this collection will store the tx with many outputs
+    result = many_inputs.find_one(tx['operationId'])
+    if not result:
+        pk = many_inputs.insert(tx)
+        if isinstance(pk, ObjectId):
+            return str(pk)
+        else:
+            return {"status": 500, "error": "Unknown server error"}
+    else:
+        return {"status": 409, "error": "Specified tx is already exists"}
