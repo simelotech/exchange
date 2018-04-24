b939c3b5bc8a005862288c53b3dc89cd6fe75d40
Ozkar L. Garcell <dev0028@simelo.tech>
2018-04-24 13:06:29 -0400

    inject tx api callback, WiP

--- exchange_app/api_1_0/transactions.py
+++ exchange_app/api_1_0/transactions.py
@@ -1,9 +1,10 @@
 import logging
-from flask import jsonify
+from flask import jsonify, request, make_response
 
 from . import api
-from .blockchain import get_unconfirmed_txs, get_tx_info, get_raw_tx
+from .blockchain import get_unconfirmed_txs, get_tx_info, get_raw_tx, inject_raw_tx
 from .. import app
+from .common import build_error
 
 
 @api.route('/transactions/unconfirmed', methods=['GET'])
@@ -15,7 +16,7 @@ def transactions_unconfirmed():
     return jsonify(result)
 
 
-@api.route('/transactions/info/<string:txid>', methods=['GET'])
+@api.route('/transaction/info/<string:txid>', methods=['GET'])
 def transactions_info(txid):
     result = get_tx_info(txid)
     if app.config['DEBUG']:
@@ -24,10 +25,25 @@ def transactions_info(txid):
     return jsonify(result)
 
 
-@api.route('/transactions/raw/<string:txid>', methods=['GET'])
+@api.route('/transaction/raw/<string:txid>', methods=['GET'])
 def transaction_raw(txid):
     result = get_raw_tx(txid)
     if app.config['DEBUG']:
         logging.debug("Transaction Info")
         logging.debug(result)
+    return jsonify(result)
+
+
+@api.route('/transaction/inject', methods=['POST'])
+def inject_raw_transaction():
+    if not request.json:
+        return make_response(jsonify(build_error("Input format error")), 400)
+    if "rawtx" not in request.json:
+        return make_response(jsonify(build_error("Input data error")), 400)
+    result = inject_raw_tx(request.json)
+    if result["status_code"] != 200 or result["error"] != "":
+        return make_response(
+            jsonify(build_error(result)),
+            400
+        )
     return jsonify(result)
