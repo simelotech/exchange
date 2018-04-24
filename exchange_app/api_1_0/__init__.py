2c4110eff54b0e9afe864eca5a20e87d6f82df61
Ozkar L. Garcell <dev0028@simelo.tech>
2018-04-23 20:17:14 -0400

    added transactions api callback to exchange app

--- exchange_app/api_1_0/__init__.py
+++ exchange_app/api_1_0/__init__.py
@@ -2,4 +2,4 @@ from flask import Blueprint
 
 api = Blueprint('api', __name__)
 
-from . import address, wallets, assets, pending_events, capabilities, isalive, common, balances, history
+from . import address, wallets, assets, pending_events, capabilities, isalive, common, balances, history, transactions
