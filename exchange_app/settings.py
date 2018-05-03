c8b9a7a5dc4165dce8ca7e01e813add44816c15e
Ozkar L. Garcell <dev0028@simelo.tech>
2018-05-03 10:54:26 -0400

    splitted settings for the three possible scenarios: dev, prod and test

--- exchange_app/settings.py
+++ exchange_app/settings.py
@@ -1,5 +1,5 @@
 import logging
-
+import uuid
 import os
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
@@ -14,35 +14,54 @@ class Config(object):
     WTF_CSRF_SECRET_KEY = '1!qaz2@wsx3#edc$4rfv'
     SITE_NAME = ''
     LOG_LEVEL = logging.DEBUG
-    SERVER_NAME = 'localhost:5000'
-    #: Database
-    MONGO_DBNAME = 'LYKKE'
-    MONGO_HOST = '127.0.0.1'
-    MONGO_PORT = 27017
-    #: Redis
-    REDIS_HOST = '127.0.0.1'
-    REDIS_PORT = 6379
-    #: SKYCOIN
-    SKYCOIN_NODE_URL = 'http://localhost:6420/'
 
 
 class ProductionConfig(Config):
     ENVIRONMENT = 'Production'
     PRODUCTION = True
     LOG_LEVEL = logging.INFO
+    #: Server Name
     SERVER_NAME = ''
+    #: Database
+    MONGO_DBNAME = ''
+    MONGO_HOST = ''
+    MONGO_PORT = 0
+    #: Redis
+    REDIS_URL = "redis://localhost:6379/0"
+    #: SKYCOIN
+    SKYCOIN_NODE_URL = ''
 
 
 class DevelopmentConfig(Config):
     ENVIRONMENT = 'Dev'
     DEBUG = True
     TESTING = False
+    #: Server Name
+    SERVER_NAME = 'localhost:5000'
+    #: Database
+    MONGO_DBNAME = 'LYKKE_DEV'
+    MONGO_HOST = '127.0.0.1'
+    MONGO_PORT = 27017
+    #: Redis
+    REDIS_URL = "redis://localhost:6379/0"
+    #: SKYCOIN
+    SKYCOIN_NODE_URL = 'http://localhost:6420/'
 
 
 class TestingConfig(Config):
     ENVIRONMENT = 'Testing'
     DEBUG = False
     TESTING = True
+    #: Server Name
+    SERVER_NAME = 'localhost:5000'
+    #: Database
+    MONGO_DBNAME = 'LYKKE_TEST_{0}'.format(str(uuid.uuid1()).replace('-', ''))
+    MONGO_HOST = '127.0.0.1'
+    MONGO_PORT = 27017
+    #: Redis
+    REDIS_URL = "redis://localhost:6379/5"
+    #: SKYCOIN
+    SKYCOIN_NODE_URL = 'http://localhost:6420/'
 
 
 environment = os.getenv('ENVIRONMENT', 'DEVELOPMENT').lower()
