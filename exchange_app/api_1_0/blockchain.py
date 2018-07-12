9008fe99e3d0cc40d3c11b4f9b4d9c3fd9e05df9
stdevEclipse <dev0003@simelo.tech>
2018-07-12 16:24:18 +0000

    [exchange] refs #95. Try to remove superflous changes.

--- exchange_app/api_1_0/blockchain.py
+++ exchange_app/api_1_0/blockchain.py
@@ -107,7 +107,7 @@ def get_version():
     Get blockchain version
     """
 
-    version = requests.get(form_url(app_config.SKYCOIN_NODE_URL, "api/v1/version"))
+    version = requests.get(form_url(app_config.SKYCOIN_NODE_URL, "/api/v1/version"))
 
     if not version.json:
         return {"status": 500, "error": "Unknown server error"}
