8d08f04961596b3fe64dac7927e41f5557bf0a49
Olemis Lang <olemis+work@gmail.com>
2018-07-23 13:29:08 -0400

    [blockchain] refs #75 - Check for valid address should consider only first value retuirned by DecodeBase58Address
    Ran 14 tests in 35.170s
    
    FAILED (failures=2)

--- exchange_app/api_1_0/address.py
+++ exchange_app/api_1_0/address.py
@@ -10,18 +10,19 @@ def address_valid(address):
     A BTC address uses an alphanumeric base58 encoding, without 0, O, I or l.
     Important note: the last four bytes are a checksum check. They are the
     first four bytes of a double SHA-256 digest of the previous 21 bytes
-    Read the first twenty-one bytes, compute the checksum, and 
+    Read the first twenty-one bytes, compute the checksum, and
     check that it corresponds to the last four bytes.
     """
 
-    
-    try: 
-        if DecodeBase58Address(address) == 0:
-            result = True
+
+    try:
+        if DecodeBase58Address(address)[0] == 0:
+             result = True
         else:
-            result = False
-        
+             result = False
+
     except Exception:
+        raise
         result = False
-        
+
     return jsonify({"isValid": result})
