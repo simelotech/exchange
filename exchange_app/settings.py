35ddc4d73a9868e2642631f999e0e2812c619ef8
Ozkar L. Garcell <dev0028@simelo.tech>
2018-08-08 16:38:53 +0000

    splitted settings.py for the three possible scenarios: dev, test and prod refs #75

--- exchange_app/settings.py
+++ exchange_app/settings.py
@@ -1,5 +1,5 @@
 import logging
-
+import uuid
 import os
 import os.path
 
@@ -10,11 +10,37 @@ class Config(object):
     DEBUG = True
     TESTING = False
     PRODUCTION = False
-
     SECRET_KEY = '1!qaz2@wsx3#edc$4rfv'
     WTF_CSRF_SECRET_KEY = '1!qaz2@wsx3#edc$4rfv'
+
+
+class ProductionConfig(Config):
+    ENVIRONMENT = 'Production'
+    PRODUCTION = True
+    LOG_LEVEL = logging.INFO
+    #: Site Name
     SITE_NAME = ''
+    SERVER_NAME = ''
+    #: Database
+    MONGO_DBNAME = ''
+    MONGO_HOST = ''
+    MONGO_PORT = 27017
+    MONGO_URI = "mongodb://"
+    #: Redis
+    REDIS_HOST = ''
+    REDIS_PORT = 6379
+    #: SKYCOIN
+    SKYCOIN_NODE_URL = ''
+    LIBSKYCOIN_PATH = ''
+
+
+class DevelopmentConfig(Config):
+    ENVIRONMENT = 'Dev'
+    DEBUG = True
+    TESTING = False
     LOG_LEVEL = logging.DEBUG
+    #: Site Name
+    SITE_NAME = ''
     SERVER_NAME = 'localhost:5000'
     #: Database
     MONGO_DBNAME = 'LYKKE'
@@ -29,23 +55,25 @@ class Config(object):
     LIBSKYCOIN_PATH = os.path.join(*('exchange_app/libskycoin.so'.split('/')))
 
 
-class ProductionConfig(Config):
-    ENVIRONMENT = 'Production'
-    PRODUCTION = True
-    LOG_LEVEL = logging.INFO
-    SERVER_NAME = ''
-
-
-class DevelopmentConfig(Config):
-    ENVIRONMENT = 'Dev'
-    DEBUG = True
-    TESTING = False
-
-
 class TestingConfig(Config):
     ENVIRONMENT = 'Testing'
     DEBUG = False
     TESTING = True
+    LOG_LEVEL = logging.DEBUG
+    #: Site Name
+    SITE_NAME = ''
+    SERVER_NAME = 'localhost:5000'
+    #: Database
+    MONGO_DBNAME = 'LYKKE_TEST_{0}'.format(str(uuid.uuid1()).replace('-', ''))
+    MONGO_HOST = '127.0.0.1'
+    MONGO_PORT = 27017
+    MONGO_URI = "mongodb://" + MONGO_HOST + ":" + str(MONGO_PORT) + "/" + MONGO_DBNAME
+    #: Redis
+    REDIS_HOST = '127.0.0.1'
+    REDIS_PORT = 6379
+    #: SKYCOIN
+    SKYCOIN_NODE_URL = 'http://localhost:6420/'
+    LIBSKYCOIN_PATH = os.path.join(*('exchange_app/libskycoin.so'.split('/')))
 
 
 environment = os.getenv('ENVIRONMENT', 'DEVELOPMENT').lower()
