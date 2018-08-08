6e21304b8d05f17c56bde83ac44827fd67d51e3b
Ozkar L. Garcell <dev0028@simelo.tech>
2018-08-08 17:01:01 +0000

    Updated some /api/transactions callbacks according to new lykke specs Refs #74

--- exchange_app/api_1_0/transactions.py
+++ exchange_app/api_1_0/transactions.py
@@ -1,38 +1,65 @@
 import logging
-from flask import request, jsonify, make_response
+from flask import jsonify, request, make_response
 
 from . import api
-from .blockchain import transaction_many_inputs, transaction_many_outputs
-from ..common import build_error
-from ..models import add_many_outputs_tx
+from .blockchain import get_unconfirmed_txs, get_tx_info, get_raw_tx, inject_raw_tx, get_tx_address_related, resend_unconfirmed_txs
 from .. import app
+from ..common import build_error
 
 
-@api.route('/api/transactions/many-inputs', methods=['POST'])
-def transactions_many_inputs():
+@api.route('/transactions/unconfirmed', methods=['GET'])
+def transactions_unconfirmed():
+    result = get_unconfirmed_txs()
+    if app.config['DEBUG']:
+        logging.debug("Unconfirmed Transactions")
+        logging.debug(len(result))
+    return jsonify(result)
+
+
+@api.route('/transaction/info/<string:txid>', methods=['GET'])
+def transactions_info(txid):
+    result = get_tx_info(txid)
+    if app.config['DEBUG']:
+        logging.debug("Transaction Info")
+        logging.debug(result)
+    return jsonify(result)
+
+
+@api.route('/transaction/raw/<string:txid>', methods=['GET'])
+def transaction_raw(txid):
+    result = get_raw_tx(txid)
+    if app.config['DEBUG']:
+        logging.debug("Transaction Info")
+        logging.debug(result)
+    return jsonify(result)
+
+
+@api.route('/transaction/inject', methods=['POST'])
+def inject_raw_transaction():
     if not request.json:
         return make_response(jsonify(build_error("Input format error")), 400)
-    params = {'operationID', 'inputs', 'toAddress', 'assetId'}
-    if all(x not in params for x in request.json):
+    if "rawtx" not in request.json:
         return make_response(jsonify(build_error("Input data error")), 400)
-    result = transaction_many_inputs(request.json)
-    if "transactionContext" in result:
-        return jsonify(result)
-    return jsonify({"status": 500, "error": "Invalid response"})
+    result = inject_raw_tx(request.json)
+    if result['status'] != 200 or result['error'] != "":
+        return make_response(
+            jsonify(build_error(result['error'])),
+            result['status']
+        )
+    return jsonify(result)
 
 
-@api.route('/api/transactions/many-outputs', methods=['POST'])
-def transactions_many_outputs():
-    if not request.json:
-        return make_response(jsonify(build_error("Input format error")), 400)
-    params = {'operationId', 'fromAddress', 'fromAddressContext', 'outputs', 'assetId'}
-    if all(x not in params for x in request.json):
-        return make_response(jsonify(build_error("Input data error")), 400)
-    tx = add_many_outputs_tx(request.json)
-    if tx:
-        result = transaction_many_outputs(request.json)
-        if "transactionContext" in result:
-            return jsonify(result)
-        if app.config['DEBUG']:
-            logging.debug("Transaction: %s", request.args.get('operationId'))
-    return jsonify({"status": 500, "error": "Invalid response"})
+@api.route(
+    '/transaction/address-related/<string:addrs>/<int:confirmed>',
+    methods=['GET']
+)
+def transactions_address_related(addrs, confirmed):
+    values = {'addrs': addrs, 'confirmed': confirmed}
+    result = get_tx_address_related(values)
+    return jsonify(result)
+
+
+@api.route('/transactions/resend', methods=['GET'])
+def transactions_resend():
+    result = resend_unconfirmed_txs()
+    return jsonify(result)
