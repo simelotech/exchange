72da3830c7a7317fcfd2d7bff12a14d265e3ed05
stdevEclipse <dev0003@simelo.tech>
2018-08-09 18:03:59 +0000

    [blockchain] refs #114 - Added more logging

--- exchange_app/signservice/sign_transaction.py
+++ exchange_app/signservice/sign_transaction.py
@@ -9,12 +9,15 @@ def sign_transaction(tx):
         form_url(app_config.SKYCOIN_NODE_URL, "/api/v1/wallet/newSeed?entropy=128")).json()
 
     if not new_seed or "seed" not in new_seed:
+        logging.debug('sign_transaction - Error creating seed')
         return {"status": 500, "error": "Unknown server error"}
     params = {'operationID', 'fromAddress', 'fromAddressContext',
             'toAddress', 'assetId', 'amount', 'includeFee'}
     if all(x not in params for x in tx):
+        logging.debug('sign_transaction - Invalid transaction context')
         return {"status": 400, "error": "Invalid transaction context"}
     if request.json['assetId'] != 'sky':
+        logging.debug('sign_transaction - Only coin is sky')
         return {"status": 400, "error": "Only coin is sky"}
     data = {
     	"hours_selection": {
@@ -39,8 +42,10 @@ def sign_transaction(tx):
         json_response = json.loads(response.get_data(as_text=True))
         if not 'error' in json_response:
             if 'encoded_transaction' in json_response:
-                {"signedTransaction" : json_response['encoded_transaction']}
+                return {"signedTransaction" : json_response['encoded_transaction']}
             else:
+                logging.debug('sign_transaction - Invalid response from create transaction"}')
                 return {"status": 500, "error": "Invalid response from create transaction"}
         else:
+            logging.debug('sign_transaction - Error from skycoin: ' + response.message)
             return {"status": response.status_code, "error": response.message}
