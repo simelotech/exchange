68be425fe9cea6a135ecc2792254ae7c45e7c6b3
Ozkar L. Garcell <dev0028@simelo.tech>
2018-08-13 14:12:11 +0000

    added support for Lykke settings interface, refs #118

--- exchange_app/__init__.py
+++ exchange_app/__init__.py
@@ -4,12 +4,33 @@ from flask import Flask
 from flask_redis import FlaskRedis
 from flask_pymongo import PyMongo
 import requests
+import json
 
 FLASK_APP_DIR = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(
     __name__
 )
+
+
+# Lykke Settings
+@app.before_first_request
+def setup_lykke_settings():
+    settings_url = os.getenv('SettingsUrl')
+    data = {}
+    if not settings_url:
+        app.logger.error("Lykke Settings not in Enviroment")
+        #: TODO What if Lykke settings are not loaded?
+    if app.config['ENVIRONMENT'] == 'Dev' or app.config['ENVIRONMENT'] == 'Testing' and 'file' in settings_url.split('://'):
+        app.logger.info("Loading Lykke Settings via {}".format(settings_url))
+        with open(settings_url.split('file://')[1]) as f:
+            data = json.load(f)
+    if app.config['ENVIRONMENT'] == 'Production':
+        app.logger.info("Loading Lykke Settings via {}".format(settings_url))
+        r = requests.get(settings_url)
+        data = r.json()
+    app.config.update(data)
+
 
 # Config
 app.config.from_object('exchange_app.settings.app_config')
@@ -23,8 +44,8 @@ logging.basicConfig(
     datefmt='%Y%m%d-%H:%M%p',
 )
 
-#: Session
-app.lykke_session = requests.Session()  #: Used for connection pooling requests to lykke api
+#: Session, Used for connection pooling requests to lykke api
+app.lykke_session = requests.Session()
 
 # Redis
 redis_store = FlaskRedis(app, strict=False)
