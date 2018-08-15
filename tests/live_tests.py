7bd1673c45e02a90857e749b2f8851571beb6eeb
stdevEclipse <dev0003@simelo.tech>
2018-08-15 11:07:10 +0000

    [blockchain] refs #114 - Added test for transactions to live test case

--- tests/live_tests.py
+++ tests/live_tests.py
@@ -16,3 +16,67 @@ class LiveTestCase(unittest.TestCase):
         text = file.read()
         seeds = json.loads(text)
         file.close()
+
+    def test_wallets(self):
+        response = self.app.post(
+            '/v1/api/wallets',
+            content_type='application/json'
+        )
+        self.assertEqual(response.status_code, 200)
+        json_response = json.loads(response.get_data(as_text=True))
+        self.assertIn('privateKey', json_response)
+        self.assertIn('publicAddress', json_response)
+        self.assertIn('addressContext', json_response)
+        fakeTx = {
+            'operationID' : '4324432444332', #fake
+            'fromAddress' : json_response['publicAddress'],
+            'fromAddressContext' : json_response['addressContext'],
+            'toAddress' : 'anyaddressthisshouldfail',
+            'assetId' : 'SKY',
+            'amount' : '1',
+            'includeFee' : False
+        }
+        #Now tests creating a transaction
+        #Result should be not enough balance
+        response = self.app.post(
+            '/v1/api/transactions/single',
+            data = json.dumps(fakeTx),
+            content_type='application/json'
+        )
+        self.assertNotEqual(response.status_code, 200)
+        fakeTx['outputs'] = [{
+            'amount' : fakeTx['amount'],
+            'toAddress' : fakeTx['toAddress']
+        }]
+        #Now tests creating a transaction with many outputs
+        #Result should be not enough balance
+        response = self.app.post(
+            '/v1/api/transactions/many-outputs',
+            data = json.dumps(fakeTx),
+            content_type='application/json'
+        )
+        self.assertNotEqual(response.status_code, 200)
+        #Test fake sign
+        data = {
+            "privateKeys" : ["3434"],
+            "transactionContext" : json.dumps(fakeTx)
+        }
+        response = self.app.post(
+            '/v1/api/sign',
+            data = json.dumps(data),
+            content_type='application/json'
+        )
+        self.assertEqual(response.status_code, 500)
+        #Test fake sign
+        #Test broadcasting fake transactions
+        #Result should be operationID not found
+        data = {
+            "operationId" : '4324432444332',
+            "signedTransaction" : 'faketrans',
+        }
+        response = self.app.post(
+            '/v1/api/transactions/broadcast',
+            data = json.dumps(data),
+            content_type='application/json'
+        )
+        self.assertEqual(response.status_code, 400)
